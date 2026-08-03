6b2ea7f8a0b35731d5c7108efe685fd4cf2b845b
MarleMGN <m0b.torres23@gmail.com>
2026-08-03 15:08:44 -0700

    animations

--- src/components/landing.tsx
+++ src/components/landing.tsx
@@ -1,20 +1,54 @@
+"use client";
 import React from "react";
 
 const Landing = () => {
+  const handleScroll = (direction: "left" | "right") => {
+    if (typeof window === "undefined") return;
+
+    window.scrollBy({
+      left: direction === "right" ? 240 : -240,
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
   return (
     <>
-      <div className="flex flex-auto flex-col relative">
-        <div style={{ right: "-425px" }} className="absolute top-1/2 -translate-y-1/2 flex items-center gap-4">
-          <span className="dotted-square__right"></span>
-          <a className="mr-8" href="">
-            <span className="order-1">
+      <div className="flex flex-auto flex-col relative overflow-hidden">
+        {/* Left Navigation */}
+        <div className="fixed left-0 top-1/2 -translate-y-1/2 z-10 flex items-center gap-2">
+          <div className="relative flex flex-col items-center">
+            <div className="relative">
+              <span className="dotted-square__left"></span>
+              <button
+                type="button"
+                className="nav-arrow nav-arrow--left"
+                onClick={() => handleScroll("left")}
+                aria-label="Discover A.I."
+              >
                 <span className="icon__button">
-                  <svg width="11" height="12" viewBox="0 0 11 12" fill="#1A1B1C" xmlns="http://www.w3.org/2000/svg"><path d="M10.145 6 .716 11.444V.556L10.145 6Z" fill="#1A1B1C"></path></svg>
+                  <svg
+                    width="11"
+                    height="12"
+                    viewBox="0 0 11 12"
+                    fill="#1A1B1C"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M10.145 6 .716 11.444V.556L10.145 6Z"
+                      fill="#1A1B1C"
+                    ></path>
+                  </svg>
                 </span>
-            </span>
-          </a>
+                <span className="text-[10px] font-medium text-[#1A1B1C] tracking-tight whitespace-nowrap pl-4.5">
+                  DISCOVER A.I.
+                </span>
+              </button>
+            </div>
+          </div>
         </div>
-        <span className="dotted-square__left"></span>
+
+        {/* Center Content */}
         <div className="px-8 w-full mr-auto ml-auto flex items-center flex-auto pb-9">
           <div className="contents">
             <div className="m-auto text-center">
@@ -34,7 +68,41 @@ const Landing = () => {
             </div>
           </div>
         </div>
+
+        {/* Right Navigation */}
+        <div className="fixed -right-94.5 top-1/2 -translate-y-1/2 z-10 flex items-center gap-2">
+          <div className="relative flex flex-col items-center">
+            <div className="relative">
+              <span className="dotted-square__right"></span>
+              <button
+                type="button"
+                className="nav-arrow nav-arrow--right"
+                onClick={() => handleScroll("right")}
+                aria-label="Take Test"
+              >
+                <span className="text-[10px] font-medium text-[#1A1B1C] tracking-tight whitespace-nowrap pr-4.5">
+                  TAKE TEST
+                </span>
+                <span className="icon__button">
+                  <svg
+                    width="11"
+                    height="12"
+                    viewBox="0 0 11 12"
+                    fill="#1A1B1C"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M10.145 6 .716 11.444V.556L10.145 6Z"
+                      fill="#1A1B1C"
+                    ></path>
+                  </svg>
+                </span>
+              </button>
+            </div>
+          </div>
+        </div>
       </div>
+      {/* </div> */}
     </>
   );
 };
